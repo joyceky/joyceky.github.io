4a756c0e7484fdcd065bc1dadd7d87ed3cc5f9a4
Kyle Joyce <kyyjoyce@gmail.com>
2016-09-28 20:01:05 -0600

    Validated html and css

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
+$(document).ready(function() {
     randomInterest();
 });
 
+var myInterests = ['building neat stuff', 'warm wooly socks', 'quiet museums', 'passport stamps',
+    'art and design', 'new web technologies', 'peat smoke', 'snuggling my cat',
+    'everything to do with the Internet', 'Netflix binges', 'typography',
+    'spotty bananas', 'Argentinian wine', 'baking bread and pastries',
+    'knitting lopsided hats', 'martial arts movies', 'creating digital art',
+    'astrophotography', 'alien civilizations', 'buttered toast',
+    'meeting new people', 'garden gnomes', 'landscape photography',
+    'hiking through clouds', 'exploring the Multiverse'
+];
 
 function randomInterest() {
-  var myInterests = ['building neat stuff', 'warm wooly socks', 'quiet museums', 'passport stamps',
-                      'art and design', 'new web technologies', 'peat smoke','snuggling my cat',
-                      'everything to do with the Internet', 'Netflix binges' , 'typography',
-                      'spotty bananas', 'Argentinian wine', 'baking bread and pastries',
-                      'knitting lopsided hats', 'martial arts movies','creating digital art',
-                      'astrophotography', 'alien civilizations', 'buttered toast',
-                      'meeting new people',  'garden gnomes', 'landscape photography',
-                      'hiking through clouds', 'exploring the Multiverse' ];
-
-  var i = setInterval(function() {
-  var num = Math.floor( Math.random() * (myInterests.length-1) );
-  $('#interests').html( myInterests[ num ] );
-}, 2500);
+    setInterval(function() {
+        var num = Math.floor(Math.random() * (myInterests.length - 1));
+        $('#interests').html('< ' + myInterests[num] + ' >');
+    }, 2500);
 }
